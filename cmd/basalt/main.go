8ecc539999564ebca277c616f76707553acabd05
Avichal Pandey <avichal2018lko@gmail.com>
2026-06-05 15:31:38 +0530

    feat: in-memory set, get, delete implemented

--- cmd/basalt/main.go
+++ cmd/basalt/main.go
@@ -1,5 +1,33 @@
 package main
 
-func main() {
+import (
+	"fmt"
 
+	"github.com/avichal-08/basalt/internal/store"
+)
+
+func main() {
+	s := store.New()
+
+	s.Set("name", "avichal")
+	s.Set("city", "lucknow")
+
+	name, ok := s.Get("name")
+	if ok {
+		fmt.Printf("name = %s\n", name)
+	}
+
+	s.Set("name", "aditya")
+
+	updatedName, ok := s.Get("name")
+	if ok {
+		fmt.Printf("updated name = %s\n", updatedName)
+	}
+
+	s.Delete("city")
+
+	_, ok = s.Get("city")
+	if !ok {
+		fmt.Println("city key not found")
+	}
 }
--- internal/store/store.go
+++ internal/store/store.go
@@ -1,1 +1,24 @@
 package store
+
+type Store struct {
+	data map[string]string
+}
+
+func New() *Store {
+	return &Store{
+		data: make(map[string]string),
+	}
+}
+
+func (s *Store) Set(key, value string) {
+	s.data[key] = value
+}
+
+func (s *Store) Get(key string) (string, bool) {
+	value, ok := s.data[key]
+	return value, ok
+}
+
+func (s *Store) Delete(key string) {
+	delete(s.data, key)
+}
